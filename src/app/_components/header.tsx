de98391cc35d25dda47229daedd5f4d753ae7490
Natthawut Haematulin <natthawut1991@gmail.com>
2025-03-07 22:26:20 +0700

    feat: update logo

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AsciiArt from "./ascii-art";
 import Link from "next/link";
 
@@ -5,9 +6,16 @@ const Header = () => {
   return (
     <div className="text-center justify-center">
       <h2 className="text-center justify-center text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
-        <Link href="/" className="hover:underline">
-          <AsciiArt fileName="coffee-mug" />
-          nux
+        <Link href="/" className="items-center">
+          <Image
+            src="mug-icon.png"
+            alt="logo"
+            width={140}
+            height={140}
+            className="filter-dark image-pixelated"
+          />
+          {/* <AsciiArt fileName="coffee-mug" /> */}
+          <span>nux&nbsp;&nbsp;</span>
         </Link>
       </h2>
     </div>
